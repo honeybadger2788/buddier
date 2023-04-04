d51223d815277b096427d731ca9bb0124d71b127
honeybadger2788 <noe.carosella@gmail.com>
2023-04-04 20:19:50 -0300

    feature: Login Activity design

--- app/src/main/java/com/example/buddier/ui/MainActivity.kt
+++ app/src/main/java/com/example/buddier/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.buddier.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import com.example.buddier.R
+import com.example.buddier.databinding.ActivityMainBinding
+import com.example.buddier.ui.loginActivity.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnLogin.setOnClickListener{
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/buddier/ui/loginActivity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.buddier.ui.loginActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.buddier.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
