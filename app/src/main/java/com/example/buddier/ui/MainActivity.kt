77ec7ffaf7e9e7c35f27a219f690c76232bd1485
Noe <72532638+honeybadger2788@users.noreply.github.com>
2023-04-04 16:50:45 -0300

    feature: Home Activity design (#1)

--- app/src/main/java/com/example/buddier/ui/MainActivity.kt
+++ app/src/main/java/com/example/buddier/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.buddier
+package com.example.buddier.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buddier.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
